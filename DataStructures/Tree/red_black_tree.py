43797144812e50fd3a2bbb757491155d8648ef02
San-FG <manuelsal5436-gmail.com>
2025-10-23 14:04:49 -0500

    Ingreso de unos requerimientos

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -17,8 +17,18 @@ def flip_node_color(node_rbt):
     return node_rbt
 
 def flip_colors(node_rbt):
+    
     flip_node_color(node_rbt)
-    flip_node_color(node_rbt["left"])
-    flip_node_color(node_rbt["right"])
+    left = flip_node_color(node_rbt["left"])
+    right = flip_node_color(node_rbt["right"])
+    node_rbt["left"] = left
+    node_rbt["right"] = right
+    
     return node_rbt
 
+def is_empty(my_rbt):
+    if my_rbt["root"] is not None:
+        return False
+    return True
+
+def 
